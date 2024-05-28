e2375587d081fc504b52766f8929daf925ff325e
AhmedFahmy22 <ahmed.fahmy00@eng-st.cu.edu.eg>
2024-05-28 14:26:24 +0300

    JS: Added operate function (takes operator and 2 operands and return result)

--- script.js
+++ script.js
@@ -7,9 +7,28 @@ function subtract(operand1, operand2) {
 }
 
 function multiply(operand1, operand2) {
-    return (Number(operand1) + Number(operand2));
+    return (Number(operand1) * Number(operand2));
 }
 
 function divide(operand1, operand2) {
     return (Number(operand1) / Number(operand2));
+}
+
+function operate(operator, operand1, operand2) {
+    let result = NaN;
+    switch(operator) {
+        case '+':
+            result = add(operand1, operand2);
+            break;
+        case '-':
+            result = subtract(operand1, operand2);
+            break;        
+        case '*':
+            result = multiply(operand1, operand2);
+            break;
+        case '/':
+            result = divide(operand1, operand2);
+            break;
+    }
+    return result;
 }
